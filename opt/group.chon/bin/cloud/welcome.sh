68685a4c2e990eca164b13af76d6de0656409129
Nilson Lazarin <nilsonmori@yahoo.com>
2024-07-03 17:56:12 -0300

    adjusting welcome output

--- opt/group.chon/bin/cloud/welcome.sh
+++ opt/group.chon/bin/cloud/welcome.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-chonosDDNSManager --status
+chonosDDNSManager --update
 
 json_ddns=$(chonosDDNSManager --status)
 domain=$(echo $json_ddns | jq -r '.domain')
 local_address=$(echo $json_ddns | jq -r '.localAddress')
 
 echo "Welcome to ChonCloud!"
-echo "Web console: "https://$domain:3270 or https://$local_address:3270"
+echo "Web console: https://$domain:3270 or https://$local_address:3270"
